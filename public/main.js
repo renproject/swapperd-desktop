8ec78773212b780a908c93a5045c39608df088bb
Jaz Gulati <jaskaran.g15@gmail.com>
2018-11-22 13:33:45 +1100

    Disabled window resizing

--- public/main.js
+++ public/main.js
@@ -12,6 +12,7 @@ const mb = menubar({
     tooltip: "Swapperd",
     showDockIcon: true, // TODO: Remove this
     preloadWindow: true,
+    resizable: false,
     webPreferences: {
         nodeIntegration: false,
         preload: __dirname + "/preload.js",
