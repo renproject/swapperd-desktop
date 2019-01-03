0757259da9759812d78b4f79cae54e0a9278a3b0
Vince Au <vinceau09@gmail.com>
2019-01-03 17:31:40 +1100

    Revert "Move icons to resources folder"
    This reverts commit f5a49f2f0b750e6b37c9268e92dd18b78aeeee4d.

--- public/main.js
+++ public/main.js
@@ -225,7 +225,7 @@ ipcMain.on("notify", (event, ...args) => {
     notifier.notify({
         title: "Swapperd",
         message: args[0],
-        icon: __dirname + "/resources/Icon.icns",
+        icon: __dirname + "/Icon.icns",
         wait: true,
     });
     event.returnValue = "";
