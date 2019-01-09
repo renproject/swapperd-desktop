247304d084a0936a7e38763d15943c8ff2d5f47f
Vince Au <vinceau09@gmail.com>
2019-01-09 11:49:09 +1100

    Initial commit using electron forge

--- public/main.js
+++ public/main.js
@@ -23,6 +23,11 @@ const {
     app
 } = require("electron");
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
 // Set app to auto-launch
 app.setLoginItemSettings({
     openAtLogin: true,
