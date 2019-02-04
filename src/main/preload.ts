250cc06dd3cd7c71050d089d993cea62603a0c10
Negaex <gh@negaex.com>
2019-02-05 10:19:35 +1100

    Preparing for merge

--- src/main/preload.ts
+++
@@ -1,2 +0,0 @@
-// @ts-ignore
-// window.ipcRenderer = require('electron').ipcRenderer;
--- src/renderer/declarations.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "bn.js";
-declare module "react-copy-to-clipboard";
-declare module '*.png';
-declare module '*.svg';
-declare module '*.jpg';
-declare module '*.jpeg';
