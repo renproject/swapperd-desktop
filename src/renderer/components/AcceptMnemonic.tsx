7a41c48daaf252cf4f6b610f79e3f75a63861370
Vince Au <vinceau09@gmail.com>
2019-02-15 12:08:54 +1100

    Await backed up confirmation on mnemonic page

--- src/renderer/components/AcceptMnemonic.tsx
+++ src/renderer/components/AcceptMnemonic.tsx
@@ -7,13 +7,21 @@ interface IAcceptMnemonicProps {
     resolve(): void;
 }
 
-export class AcceptMnemonic extends React.Component<IAcceptMnemonicProps> {
+interface IAcceptMnemonicState {
+    accepted: boolean;
+}
+
+export class AcceptMnemonic extends React.Component<IAcceptMnemonicProps, IAcceptMnemonicState> {
     constructor(props: IAcceptMnemonicProps) {
         super(props);
         this.onAccept = this.onAccept.bind(this);
+        this.state = {
+            accepted: false
+        };
     }
 
     public render() {
+        const { accepted } = this.state;
         const { mnemonic } = this.props;
         return (
             <>
@@ -21,10 +29,20 @@ export class AcceptMnemonic extends React.Component<IAcceptMnemonicProps> {
                 <div className="mnemonic">
                     <p>Please back-up the following 12 words securely. You can restore your account using these words and the account details you used to sign-up.</p>
                     <textarea disabled={true} value={mnemonic} />
-                    <button onClick={this.onAccept}>Continue</button>
+                    <div className="confirmation">
+                        <label>
+                            <input name="isGoing" type="checkbox" checked={this.state.accepted} onChange={this.handleInputChange} />
+                            I have backed up my 12 words and understand that I am solely responsible for any loss of funds due to their failed safekeeping.
+                        </label>
+                    </div>
+                    <button onClick={this.onAccept} disabled={!accepted}>Continue</button>
                 </div>
             </>
         );
+    }
+
+    private handleInputChange = (): void => {
+        this.setState({ accepted: !this.state.accepted });
     }
 
     private onAccept(): void {
