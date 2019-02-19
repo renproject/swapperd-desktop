a6509a74181288a02c4e67415dc3e645562b7aa2
Vince Au <vinceau09@gmail.com>
2019-02-19 11:10:39 +1100

    Emphasis password backup and separate backup confirmation checkboxes

--- src/renderer/components/AcceptMnemonic.tsx
+++ src/renderer/components/AcceptMnemonic.tsx
@@ -8,44 +8,52 @@ interface IAcceptMnemonicProps {
 }
 
 interface IAcceptMnemonicState {
+    backedUp: boolean;
     accepted: boolean;
 }
 
 export class AcceptMnemonic extends React.Component<IAcceptMnemonicProps, IAcceptMnemonicState> {
     constructor(props: IAcceptMnemonicProps) {
         super(props);
-        this.onAccept = this.onAccept.bind(this);
         this.state = {
-            accepted: false
+            backedUp: false,
+            accepted: false,
         };
     }
 
     public render() {
-        const { accepted } = this.state;
+        const { accepted, backedUp } = this.state;
         const { mnemonic } = this.props;
         return (
             <>
                 <Banner title="Account created" />
                 <div className="mnemonic">
-                    <p>Please back-up the following 12 words securely. You can restore your account using these words and the account details you used to sign-up.</p>
+                    <p>Please back-up the following 12 words securely. To restore your account, you will need <b>BOTH</b> your password and the following 12 words.</p>
                     <textarea disabled={true} value={mnemonic} />
                     <div className="confirmation">
                         <label>
-                            <input name="isGoing" type="checkbox" checked={this.state.accepted} onChange={this.handleInputChange} />
-                            I have backed up my 12 words and understand that I am solely responsible for any loss of funds due to their failed safekeeping.
+                            <input type="checkbox" name="backedUp" checked={this.state.backedUp} onChange={this.handleInputChange} />
+                            I have backed up <b>BOTH</b> my password and my 12 words.
                         </label>
                     </div>
-                    <button onClick={this.onAccept} disabled={!accepted}>Continue</button>
+                    <div className="confirmation">
+                        <label>
+                            <input type="checkbox" name="accepted" checked={this.state.accepted} onChange={this.handleInputChange} />
+                            I understand that I am solely responsible for any loss of funds due to their failed safekeeping.
+                        </label>
+                    </div>
+                    <button onClick={this.onAccept} disabled={!accepted || !backedUp}>Continue</button>
                 </div>
             </>
         );
     }
 
-    private handleInputChange = (): void => {
-        this.setState({ accepted: !this.state.accepted });
+    private handleInputChange = (event: React.FormEvent<HTMLInputElement>): void => {
+        const element = (event.target as HTMLInputElement);
+        this.setState((state) => ({ ...state, [element.name]: !this.state[element.name] }));
     }
 
-    private onAccept(): void {
+    private onAccept = (): void => {
         this.props.resolve();
     }
 }
