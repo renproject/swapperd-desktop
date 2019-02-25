574c688c7a3c17ed6aebe93d5d2a4ab72eab0cb6
Vince Au <vinceau09@gmail.com>
2019-02-25 13:22:58 +1100

    Re-add type safety to connect function

--- src/renderer/store/connect.tsx
+++ src/renderer/store/connect.tsx
@@ -10,31 +10,13 @@ export interface ConnectedProps {
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-/*
-export interface ConnectedProps {
-    container: AppContainer;
-}
-
-// Typesafe version of https://github.com/goncy/unstated-connect
-export function connect<X extends ConnectedProps>(_container: typeof AppContainer) {
-    return (Component: React.ComponentClass<X>) => (props: Omit<X, "container">) => (
-        <Subscribe to={[_container]}>
-            {(...containers) => <Component {...({ ...props, container: containers[0] } as unknown as X)} />}
+// Somewhat typesafe version of https://github.com/goncy/unstated-connect
+export function connect<X extends ConnectedProps>(_containers: any[]) {
+    return (Component: React.ComponentClass<X>) => (props: Omit<X, "containers">) => (
+        <Subscribe to={_containers}>
+            {(...containers) => <Component {...({ ...props, containers } as unknown as X)} />}
         </Subscribe>
     );
-}
-*/
-
-export function connect<X extends ConnectedProps>(_containers: any) {
-    return (Component: any) => {
-        return (props: Omit<X, "containers">) => {
-            return (
-                <Subscribe to={_containers}>
-                    {(...containers) => <Component {...props} containers={containers} />}
-                </Subscribe>
-            );
-        };
-    };
 }
 
 // tslint:enable:no-any
